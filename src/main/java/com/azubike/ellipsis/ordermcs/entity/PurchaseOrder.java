0c10ddd79c27e23b1cd5ae778740286636f8cba1
Enu Azubike <enuazubike88@gmail.com>
2023-02-06 09:09:17 +0100

    added entity class

---
+++ src/main/java/com/azubike/ellipsis/ordermcs/entity/PurchaseOrder.java
@@ -0,0 +1,24 @@
+package com.azubike.ellipsis.ordermcs.entity;
+
+import com.azubike.ellipsis.ordermcs.dto.response.OrderStatus;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class PurchaseOrder {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  private Integer userId;
+  private Integer productId;
+  private double amount;
+  private OrderStatus orderStatus;
+}
