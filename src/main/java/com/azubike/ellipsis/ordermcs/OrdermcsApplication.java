34b74da2808df571b49140ba0e976bfd6948e741
Enu Azubike <enuazubike88@gmail.com>
2023-02-06 02:32:30 +0100

    setup packages

---
+++ src/main/java/com/azubike/ellipsis/ordermcs/OrdermcsApplication.java
@@ -0,0 +1,13 @@
+package com.azubike.ellipsis.ordermcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdermcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdermcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azubike/ellipsis/ordermcs/OrdermcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azubike.ellipsis.ordermcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdermcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
